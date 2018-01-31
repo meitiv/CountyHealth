066535863d02b956017fd1871f3d959083e040bf
meitiv <alobkovsky@gmail.com>
2018-01-30 21:18:20 -0500

    Switched to pandas dataframe

--- make_clean_dataset.py
+++ make_clean_dataset.py
@@ -1,36 +1,57 @@
 #!/usr/bin/env python3
 
+# make the dataframe
+import pandas as pd
+cols = ['Cancer_Incidence',
+        'Population_Size',
+        'Population_Density',
+        'Poverty',
+        'White',
+        'Black',
+        'Hispanic',
+        'Native_American',
+        'Asian',
+        'No_Exercise',
+        'Few_Fruit_Veg',
+        'Obesity',
+        'High_Blood_Pres',
+        'Smoker',
+        'Diabetes',
+        'Uninsured',
+        'Prim_Care_Phys_Rate',
+        'Dentist_Rate',
+        'Unemployed',
+        'Major_Depression',
+        'Recent_Drug_Use',
+        'Ecol_Rpt',
+        'Salm_Rpt',
+        'Shig_Rpt',
+        'Toxic_Chem',
+        'Sulfur_Dioxide_Ind',
+        'Ozone_Ind',
+        'Particulate_Matter_Ind',
+        'Lead_Ind']
+
+data = pd.DataFrame(columns = cols)
 # read the cancer incidence
 import csv
-cancerIncidence = {}
 with open('incd.csv', newline='') as f:
     reader = csv.reader(f)
     for row in reader:
-        cancerIncidence[row[1].strip()] = row[3].strip()
+        key = row[1].strip()
+        incidence = row[3].strip()
+        data.set_value(key,'Cancer_Incidence', incidence)
 
 # read the demographic data
-popSize = {}
-popDens = {}
-poverty = {}
-fracCAU = {}
-fracAA = {}
-fracHIS = {}
-fracNA = {}
-fracASI = {}
-with open('chsi_dataset/DEMOGRAPHIC.csv', newline='') as f:
+with open('chsi_dataset/DEMOGRAPHICS.csv', newline='') as f:
     reader = csv.DictReader(f)
     for row in reader:
         key = row['State_FIPS_Code'] + row['County_FIPS_Code']
-        popSize[key] = row['Population_Size']
-        popDens[key] = row['Population_Density']
-        poverty[key] = row['Poverty']
-        fracCAU[key] = row['White']
-        fracAA[key] = row['Black']
-        fracHIS[key] = row['Hispanic']
-        fracNA[key] = row['Native_American']
-        fracASI[key] = row['Asian']
+        for col in cols:
+            if col in row:
+                data.set_value(key, col, row[col])
 
-def padZero(text,lenght):
+def padZero(text,length):
     l = len(text)
     if l < length:
         return '0'*(length - l) + text
@@ -38,30 +59,15 @@ def padZero(text,lenght):
         return text
     
 # read in the health factors
-sedentary = {}
-poor_diet = {}
-obesity = {}
-hypertension = {}
-smoker = {}
-diabetic = {}
-uninsured = {}
-primary_care = {}
-dentist = {}
 with open('chsi_dataset/RISKFACTORSANDACCESSTOCARE.csv', newline='') as f:
     reader = csv.DictReader(f)
     for row in reader:
         fipsState = padZero(row['State_FIPS_Code'],2)
         fipsCounty = padZero(row['County_FIPS_Code'],3)
         key = fipsState + fipsCounty
-        sedentary[key] = row['No_Exercise']
-        poor_diet[key] = row['Few_Fruit_Veg']
-        obesity[key] = row['Obesity']
-        hypertension[key] = 
-        smoker[key] = row['Smoker']
-        diabetic[key] = row['Diabetes']
-        uninsured[key] = row['Uninsured']
-        primary_care[key] = row['Prim_Care_Phys_Rate']
-        dentist[key] = row['Dentist_Rate']
+        for col in cols:
+            if col in row:
+                data.set_value(key, col, row[col])
 
 # read in exposure
 with open('chsi_dataset/VUNERABLEPOPSANDENVHEALTH.csv', newline='') as f:
@@ -70,4 +76,8 @@ with open('chsi_dataset/VUNERABLEPOPSANDENVHEALTH.csv', newline='') as f:
         fipsState = padZero(row['State_FIPS_Code'],2)
         fipsCounty = padZero(row['County_FIPS_Code'],3)
         key = fipsState + fipsCounty
-        
+        for col in cols:
+            if col in row:
+                data.set_value(key, col, row[col])
+
+data.to_csv('clean_data.csv')        
